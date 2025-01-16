79024551f2518d48e5187c1badad08ffa153ddf6
Markus Murakaru <mmurakaru@gmail.com>
2025-01-16 21:39:01 +0100

    Setup meli.js

---
+++ README.md
@@ -0,0 +1,7 @@
+## meli.js
+
+Simple meli.js installation via npm. Suitable for modern web apps that use build systems such as Webpack.
+
+The full meli.js script lives on our CDN at:
+
+https://meli-tour.vercel.app/bundle.min.js
---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+
+const plugins = [
+  nodeResolve({
+    jsnext: true,
+  }),
+  typescript({
+    sourceMap: false,
+  }),
+  terser(),
+];
+
+export default {
+  input: "src/meli.ts",
+  output: {
+    file: "out/bundle.min.js",
+    format: "es",
+    sourcemap: "inline",
+  },
+  plugins,
+};
