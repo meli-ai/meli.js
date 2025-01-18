878c32b16bfec1f7cae379f9648169b9e45b0942
Markus Murakaru <mmurakaru@gmail.com>
2025-01-18 18:57:51 +0100

    wip

--- src/meli.ts
+++ src/meli.ts
@@ -7,61 +7,130 @@ interface WindowWithMeli extends Window {
 interface Meli {
   _stubbed: boolean;
   load: () => Promise<void>;
-  init: (...args: any[]) => Promise<void>;
-  start: (...args: any[]) => Promise<void>;
-  end: (...args: any[]) => Promise<void>;
-  auth: (...args: any[]) => Promise<void>;
-  authAnonymous: (...args: any[]) => Promise<void>;
+  init: (tourId: string) => Promise<void>;
+  start: () => Promise<void>;
+  end: () => Promise<void>;
+  auth: () => Promise<void>;
+  authAnonymous: () => Promise<void>;
   processQueue: () => void;
-  [key: string]: any; // This allows for dynamic method addition
+  [key: string]: any;
 }
 
+type MeliLoadState = "NOT_LOADED" | "LOADING" | "LOADED";
+
+// Window with meli defined
 const w: WindowWithMeli = typeof window === "undefined" ? ({} as any) : window;
 let meli: Meli | undefined = w.meli;
 
+console.log("Is Meli loaded:", meli);
+
 if (!meli) {
-  const MELI_QUEUE: Array<[string, any[]]> = [];
+  const MELI_QUEUE: Array<[string, string]> = [];
   const urlPrefix = "https://meli-tour.vercel.app/";
-  let loadPromise: Promise<void> | null = null;
+  let loadState: MeliLoadState = "NOT_LOADED";
+  let queueProcessed = false;
 
   meli = w.meli = {
     _stubbed: true,
     load: function () {
-      if (!loadPromise) {
-        loadPromise = new Promise(function (resolve, reject) {
-          const script = document.createElement("script");
-          script.async = true;
-          script.src = `${urlPrefix}bundle.min.js`;
-          script.onload = () => resolve();
-          script.onerror = (e) => reject(e);
-          document.head.appendChild(script);
-        });
-      }
-      return loadPromise;
+      return new Promise<void>((resolve, reject) => {
+        if (loadState === "LOADING" || loadState === "LOADED") {
+          return; // Avoid loading if already in progress or loaded
+        }
+
+        loadState = "LOADING";
+
+        const script = document.createElement("script");
+        script.type = "module";
+        script.async = true;
+        script.src = `${urlPrefix}bundle.min.js`;
+
+        script.onload = function () {
+          console.log("Meli script loaded successfully");
+          loadState = "LOADED";
+          meli!._stubbed = false;
+          resolve();
+        };
+
+        script.onerror = function () {
+          loadState = "NOT_LOADED";
+          reject(new Error("Could not load meli.js"));
+        };
+
+        document.head.appendChild(script);
+      });
     },
   } as Meli;
 
+  if (typeof window !== "undefined" && meli._stubbed) {
+    console.log("Loading meli...");
+    meli
+      .load()
+      .then(() => meli!.processQueue())
+      .catch(console.error);
+  }
+
   const stubMethod = function (name: string) {
-    meli![name] = function (...args: any[]) {
-      meli!.load().then(function () {
-        w.meli![name].apply(w.meli, args);
-      });
-      MELI_QUEUE.push([name, args]);
-    };
+    let isCallingMethod = false;
+
+    if (meli) {
+      meli[name] = function (tourId: string) {
+        if (isCallingMethod) return Promise.resolve(); // Prevent recursive calls
+
+        console.log(`${name} called with argument:`, tourId);
+        isCallingMethod = true;
+
+        if (meli!._stubbed) {
+          MELI_QUEUE.push([name, tourId]);
+
+          if (loadState === "NOT_LOADED" && !queueProcessed) {
+            console.log("Loading Meli...");
+            meli!
+              .load()
+              .then(() => meli!.processQueue())
+              .catch(console.error);
+          }
+          isCallingMethod = false;
+          return Promise.resolve();
+        } else {
+          return meli![name](tourId).finally(() => {
+            isCallingMethod = false;
+          });
+        }
+      };
+    } else {
+      console.error("Meli is undefined while assigning method:", name);
+    }
   };
 
-  stubMethod("init");
-  stubMethod("start");
-  stubMethod("end");
-  stubMethod("auth");
-  stubMethod("authAnonymous");
+  ["init", "start", "end", "auth", "authAnonymous"].forEach(stubMethod);
+
+  let isQueueProcessed = false;
 
   meli.processQueue = function () {
-    MELI_QUEUE.forEach(function (item) {
-      w.meli![item[0]].apply(w.meli, item[1]);
-    });
-    MELI_QUEUE.length = 0;
-    meli!._stubbed = false;
+    if (queueProcessed || loadState !== "LOADED" || isQueueProcessed) {
+      console.log(
+        "Queue already processed or Meli not fully loaded or already processed."
+      );
+      return;
+    }
+
+    console.log("Processing queue");
+    isQueueProcessed = true;
+    queueProcessed = true;
+    while (MELI_QUEUE.length > 0) {
+      const [method, tourId] = MELI_QUEUE.shift()!;
+      console.log(`Executing queued method: ${method} with tourId:`, tourId);
+      try {
+        if (meli) {
+          meli[method](tourId);
+        } else {
+          console.error("Meli is undefined while processing the queue.");
+        }
+      } catch (error) {
+        console.error(`Error processing queued method ${method}:`, error);
+      }
+    }
   };
 }
 
